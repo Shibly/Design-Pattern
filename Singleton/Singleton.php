f42e1337c12c1e350375643341bfaaf558129ec7
Shibly <shibly.phy@gmail.com>
2013-07-12 02:43:55 +0600

    deleted
    deleted the .DS_Store

--- Singleton/Singleton.php
+++ Singleton/Singleton.php
@@ -1,36 +1,112 @@
 <?php
 
-interface ISingleton {
+class BookSingleton {
 
-    static function getInstance();
-}
-
-class Database extends PDO implements Singleton {
-
-    private static $instance;
+    private $author = 'Gamma, Helm, Johnson, and Vlissides';
+    private $title = 'Design Patterns';
+    private static $book = NULL;
+    private static $isLoanedOut = FALSE;
 
     private function __construct() {
-        parent::__construct("mysql:host=localhost;dbname=database", "user", "password");
+        
     }
 
-    public static function getInstance() {
-        if (self::$instance === null) {
-            self::$instance = new Database();
+    static function borrowBook() {
+        if (FALSE == self::$isLoanedOut) {
+            if (NULL == self::$book) {
+                self::$book = new BookSingleton();
+            }
+            self::$isLoanedOut = TRUE;
+            return self::$book;
+        } else {
+            return NULL;
         }
-        return self::$instance;
     }
 
-    public function __clone() {
-        die(__CLASS__ . "Class can't be instantiated. Please use the method called getInstance");
+    function returnBook(BookSingleton $bookReturned) {
+        self::$isLoanedOut = FALSE;
+    }
+
+    function getAuthor() {
+        return $this->author;
+    }
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getAuthorAndTitle() {
+        return $this->getTitle() . ' by ' . $this->getAuthor();
     }
 
 }
 
-// Usages
+class BookBorrower {
 
+    private $borrowedBook;
+    private $haveBook = FALSE;
 
-$db1 = Database::getInstance();
-$db1->prepare("Select * from table");
-// $db2 is the same instance of $db1
-$db2 = Database::getInstance();
+    function __construct() {
+        
+    }
+
+    function getAuthorAndTitle() {
+        if (TRUE == $this->haveBook) {
+            return $this->borrowedBook->getAuthorAndTitle();
+        } else {
+            return "I don't have the book";
+        }
+    }
+
+    function borrowBook() {
+        $this->borrowedBook = BookSingleton::borrowBook();
+        if ($this->borrowedBook == NULL) {
+            $this->haveBook = FALSE;
+        } else {
+            $this->haveBook = TRUE;
+        }
+    }
+
+    function returnBook() {
+        $this->borrowedBook->returnBook($this->borrowedBook);
+    }
+
+}
+
+/*
+ *   Initialization
+ */
+
+writeln('BEGIN TESTING SINGLETON PATTERN');
+writeln('');
+
+$bookBorrower1 = new BookBorrower();
+$bookBorrower2 = new BookBorrower();
+
+$bookBorrower1->borrowBook();
+writeln('BookBorrower1 asked to borrow the book');
+writeln('BookBorrower1 Author and Title: ');
+writeln($bookBorrower1->getAuthorAndTitle());
+writeln('');
+
+$bookBorrower2->borrowBook();
+writeln('BookBorrower2 asked to borrow the book');
+writeln('BookBorrower2 Author and Title: ');
+writeln($bookBorrower2->getAuthorAndTitle());
+writeln('');
+
+$bookBorrower1->returnBook();
+writeln('BookBorrower1 returned the book');
+writeln('');
+
+$bookBorrower2->borrowBook();
+writeln('BookBorrower2 Author and Title: ');
+writeln($bookBorrower1->getAuthorAndTitle());
+writeln('');
+
+writeln('END TESTING SINGLETON PATTERN');
+
+function writeln($line_in) {
+    echo $line_in . '<br/>';
+}
 ?>
